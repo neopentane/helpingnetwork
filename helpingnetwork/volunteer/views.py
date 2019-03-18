16fbd31168d654a33de76025e9c5fc4f42c3d72f
neopentane <shreeramkelkar7@gmail.com>
2019-03-18 17:15:56 +0530

    Added City First Name and Last Name

--- helpingnetwork/volunteer/views.py
+++ helpingnetwork/volunteer/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+#from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
 
 def register(request):
-	form=UserCreationForm()
-	return render(request,'volunteer/register.html',{'form':form})
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}!')
+			return redirect('register')
+	else:
+			form = UserRegisterForm()
+	return render(request, 'volunteer/register.html', {'form': form})
