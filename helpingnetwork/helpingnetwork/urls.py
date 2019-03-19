1ab0865c84c506bdec7a8b76c926743d001cf301
neopentane <shreeramkelkar7@gmail.com>
2019-03-19 07:52:12 +0530

    added Organization

--- helpingnetwork/helpingnetwork/urls.py
+++ helpingnetwork/helpingnetwork/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('volunteer.urls'),),
+    path('org', include('organization.urls'),),
 ]
