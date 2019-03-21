263f632885939e9271d33bc7d74c093631d9b23e
neopentane <shreeramkelkar7@gmail.com>
2019-03-21 08:20:27 +0530

    Added Organization Creation Form

---
+++ helpingnetwork/organization/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Organization,Event
+
+
+class OrganizationRegisterForm(UserCreationForm):
+	name=forms.CharField(required=True, label="Organization Name")
+	email = forms.EmailField()
+	disp = forms.CharField(max_length=200, widget=forms.TextInput({}),label="description")
+	class Meta:
+		model = User
+		fields = ['username','email','password1','password2','name','disp']
+
+class OrganizationUpdate(forms.ModelForm):	
+	class Meta:
+		model=Organization		
+		fields = ['name']
--- helpingnetwork/organization/views.py
+++ helpingnetwork/organization/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import OrganizationRegisterForm
+from .models import Organization
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def signup(request):
-	return render(request, 'volunteer/signup.html', {'form': "ASdjhasj"})
+	if request.method == 'POST':
+		form = OrganizationRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			o_name = form.cleaned_data.get('name')
+			o_desp= form.cleaned_data.get('disp')
+			t_user=User.objects.filter(username=username).first()
+			p=Organization(user=t_user,name=o_name,description=o_desp)
+			p.save()
+			messages.success(request, f'Account created for {username}!')
+			return redirect('register')
+	else:
+			form = OrganizationRegisterForm()
+	return render(request, 'volunteer/signup.html', {'form': form})
+
